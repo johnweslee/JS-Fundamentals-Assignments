9e127f7d4df9430e130093cd8962e23dae6aeb2f
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-11 22:45:43 +0530

    update: values and variables

---
+++ Assignments/assignment.js
@@ -0,0 +1,8 @@
+//values and variables
+
+let country, continent, population;
+country = 'India';
+continent = 'Asia';
+population = 143554646;
+
+console.log(`Country: ${country}, Continent: ${continent}, Population: ${population}`);
