38ace89686b94a6be4d143dec86660499159b7d6
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-11 23:14:52 +0530

    fix: updated for code for error - 'SyntaxError: Missing initializer in const declaration'

--- Assignments/assignment.js
+++ Assignments/assignment.js
@@ -1,9 +1,9 @@
 //values and variables
 
-const country, continent;
+const country = 'India';
+const continent = 'Asia';
 let population;
-country = 'India';
-continent = 'Asia';
+
 population = 143554646;
 
 console.log(`Country: ${country}, Continent: ${continent}, Population: ${population}`);
