fd9fb6e273d5d3c7b34a1b289eba2a12f6e9e43c
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-12 03:13:53 +0530

    uncomment

--- Assignments/assignment.js
+++ Assignments/assignment.js
@@ -1,117 +1,117 @@
-// //Part 1
+//Part 1
 
-// //values and variables
+//values and variables
 
-// const country = 'India';
-// const continent = 'Asia';
-// let population;
+const country = 'India';
+const continent = 'Asia';
+let population;
 
-// population = 143554646;
+population = 143554646;
 
-// console.log(`Country: ${country}, Continent: ${continent}, Population: ${population}`);
+console.log(`Country: ${country}, Continent: ${continent}, Population: ${population}`);
 
-// //data types
+//data types
 
-// let isIsland = true;
+let isIsland = true;
 
-// //let, const, var
-// const language = 'Tamizh';
+//let, const, var
+const language = 'Tamizh';
 
-// console.log(typeof isIsland, typeof population, typeof country, typeof language);
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
 
-// //basic operators
-// let halfCountry = population / 2;
-// console.log(`Half the population of ${country} is ${halfCountry}`);
+//basic operators
+let halfCountry = population / 2;
+console.log(`Half the population of ${country} is ${halfCountry}`);
 
-// population += 1;
+population += 1;
 
-// let finlandPopulation = 6000000;
-// console.log(`Finland has a population of ${finlandPopulation}. Does ${country} have more people than Finland - ${population > finlandPopulation}`);
+let finlandPopulation = 6000000;
+console.log(`Finland has a population of ${finlandPopulation}. Does ${country} have more people than Finland - ${population > finlandPopulation}`);
 
-// let avgPopulation = 33000000;
-// console.log(`Does ${country} have less people than the average country - ${population < avgPopulation}`);
+let avgPopulation = 33000000;
+console.log(`Does ${country} have less people than the average country - ${population < avgPopulation}`);
 
-// //Strings and Template Literals
-// let description = `${country} is in ${continent}, and its ${halfCountry} people speak ${language}`;
-// console.log(description);
+//Strings and Template Literals
+let description = `${country} is in ${continent}, and its ${halfCountry} people speak ${language}`;
+console.log(description);
 
-// //taking decisions : if/else statements
-// if(population > 33000000){
-//     console.log(`${country}'s population is above avarage`);
-// }else{
-//     console.log(`${country}'s population is ${population} million below average`);
-// }
+//taking decisions : if/else statements
+if(population > 33000000){
+    console.log(`${country}'s population is above avarage`);
+}else{
+    console.log(`${country}'s population is ${population} million below average`);
+}
 
-// let population1 = 13000000;
-// if(population1 > 33000000){
-//     console.log(`${country}'s population is above avarage`);
-// }else{
-//     console.log(`${country}'s population is ${population1} million below average`);
-// }
+let population1 = 13000000;
+if(population1 > 33000000){
+    console.log(`${country}'s population is above avarage`);
+}else{
+    console.log(`${country}'s population is ${population1} million below average`);
+}
 
-// let population2 = 130000000
-// if(population2 > 33000000){
-//     console.log(`${country}'s population is above avarage`);
-// }else{
-//     console.log(`${country}'s population is ${population2} million below average`);
-// }
+let population2 = 130000000
+if(population2 > 33000000){
+    console.log(`${country}'s population is above avarage`);
+}else{
+    console.log(`${country}'s population is ${population2} million below average`);
+}
 
-// //Type Conversion and Coercion
-// console.log(`Output for the operation - '9'-'5', expected: 4, actual:${'9'-'5'}`);
-// console.log(`Output for the operation - '19'-'13'+'17', expected: 617, actual:${'19'-'13'+'17'}`);
-// console.log(`Output for the operation - '19'-'13'+17, expected: 23, actual:${'19'-'13'+17}`);
-// console.log(`Output for the operation - '123'<57, expected: false, actual:${'123'<57}`);
-// console.log(`Output for the operation - 5+6+'4'+9-4-2, expected: 1143, actual:${5+6+'4'+9-4-2}`);
+//Type Conversion and Coercion
+console.log(`Output for the operation - '9'-'5', expected: 4, actual:${'9'-'5'}`);
+console.log(`Output for the operation - '19'-'13'+'17', expected: 617, actual:${'19'-'13'+'17'}`);
+console.log(`Output for the operation - '19'-'13'+17, expected: 23, actual:${'19'-'13'+17}`);
+console.log(`Output for the operation - '123'<57, expected: false, actual:${'123'<57}`);
+console.log(`Output for the operation - 5+6+'4'+9-4-2, expected: 1143, actual:${5+6+'4'+9-4-2}`);
 
-// //equality operators: == vs ===
-// //let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
-// let numNeighbours = 9;
-// console.log(`How many neighbours does your country have?`);
-// if(numNeighbours === 1){
-//     console.log(`Only ${numNeighbours} border`);
-// }else if(numNeighbours > 1){
-//     console.log(`More than 1 border`);
-// }else{
-//     console.log(`No borders`);
-// }
+//equality operators: == vs ===
+//let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+let numNeighbours = 9;
+console.log(`How many neighbours does your country have?`);
+if(numNeighbours === 1){
+    console.log(`Only ${numNeighbours} border`);
+}else if(numNeighbours > 1){
+    console.log(`More than 1 border`);
+}else{
+    console.log(`No borders`);
+}
 
-// //logical operators
-// //Sarah's criteria
-// if(population < 50000000 && language === 'English' && !isIsland){
-//     console.log(`You should live in ${country}`);
-// }else{
-//     console.log(`${country} does not meet your criteria :(`);
-// }
+//logical operators
+//Sarah's criteria
+if(population < 50000000 && language === 'English' && !isIsland){
+    console.log(`You should live in ${country}`);
+}else{
+    console.log(`${country} does not meet your criteria :(`);
+}
 
-// //Ternary operator
-// let result = population > 33000000 ? `${country}'s population is above average` : `${country}'s population is below average`;
-// console.log(result);
-// let population3 = 130000000
-// result = population3 > 33000000 ? `${country}'s population is above average` : `${country}'s population is below average`;
-// console.log(result);
+//Ternary operator
+let result = population > 33000000 ? `${country}'s population is above average` : `${country}'s population is below average`;
+console.log(result);
+let population3 = 130000000
+result = population3 > 33000000 ? `${country}'s population is above average` : `${country}'s population is below average`;
+console.log(result);
 
-// //switch statement
-// switch(language){
-//     case 'Mandarin':
-//     case 'Chinese':
-//         console.log(`MOST number of native speakers!`);
-//         break;
-//     case 'Spanish':
-//         console.log(`2nd place in number of native speakers`);
-//         break;
-//     case 'English':
-//         console.log(`3rd place`);
-//         break;
-//     case 'Hindi':
-//         console.log(`Number 4`);
-//         break;
-//     case 'Arabic':
-//         console.log(`5th most spoken language`);
-//         break;
-//     default:
-//         console.log(`Great language too :D`);
-//         break;
-// }
+//switch statement
+switch(language){
+    case 'Mandarin':
+    case 'Chinese':
+        console.log(`MOST number of native speakers!`);
+        break;
+    case 'Spanish':
+        console.log(`2nd place in number of native speakers`);
+        break;
+    case 'English':
+        console.log(`3rd place`);
+        break;
+    case 'Hindi':
+        console.log(`Number 4`);
+        break;
+    case 'Arabic':
+        console.log(`5th most spoken language`);
+        break;
+    default:
+        console.log(`Great language too :D`);
+        break;
+}
 
 // Part 2
 
