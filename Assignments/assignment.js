0e29ecfccd601a0c6f60fdb7e6912853d8623423
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-11 23:36:50 +0530

    update: new task

--- Assignments/assignment.js
+++ Assignments/assignment.js
@@ -16,3 +16,19 @@ let isIsland = true;
 const language = 'Tamizh';
 
 console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
+//basic operators
+let halfCountry = population / 2;
+console.log(`Half the population of ${country} is ${halfCountry}`);
+
+population += 1;
+
+let finlandPopulation = 6000000;
+console.log(`Finland has a population of ${finlandPopulation}. Does ${country} have more people than Finland - ${population > finlandPopulation}`);
+
+let avgPopulation = 33000000;
+console.log(`Does ${country} have less people than the average country - ${population < avgPopulation}`);
+
+//Strings and Template Literals
+let description = `${country} is in ${continent}, and its ${halfCountry} people speak ${language}`;
+console.log(description);
