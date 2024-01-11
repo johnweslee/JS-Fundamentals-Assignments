24573e0f8c5d7b1dc209e8365e487f32f7b4de36
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-11 22:58:50 +0530

    update: data types

--- Assignments/assignment.js
+++ Assignments/assignment.js
@@ -6,3 +6,10 @@ continent = 'Asia';
 population = 143554646;
 
 console.log(`Country: ${country}, Continent: ${continent}, Population: ${population}`);
+
+//data types
+
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
