3efd1784766c9a46e003d84db82a57c49a43d0cf
John Weslin M <36557499+johnweslee@users.noreply.github.com>
2024-01-13 12:50:28 +0530

    Update Problem statements forecastTemperature.js

--- Challenges/forecastTemperature.js
+++ Challenges/forecastTemperature.js
@@ -1,1 +1,11 @@
+// Codiing challenge #1
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
 
+Example : [17, 21, 23] will print "... 17°C i 1 days ... 21°C in 2 days ... 23°C in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+TEST DATA 1 : [17, 21, 23]
+TEST DATA 2 : [12, 5, -5, 0, 4] 
+*/
